10aa33320c433280c9c2492f90f29ef27560fb9d
SantiagoHM27 <himejimadarkrai@gmail.com>
2024-03-07 09:52:27 -0600

    RadioButton

--- lib/screens/inputs.dart
+++ lib/screens/inputs.dart
@@ -10,33 +10,42 @@ class Inputs extends StatefulWidget {
 
 class _InputsState extends State<Inputs> {
   bool valueSwitch = false;
-  double sliderValue = 5.0;
-  
+  double sliderValue = 0.0;
+  int foodRadio = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Entradas')
-        ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          entradaTexto(),
-          entradaSwitch(),
-          entradaSider(),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: null,
-                child: Text('Regresar')
+        title: const Text('Entradas'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            entradaTexto(),
+            entradaSwitch(),
+            entradaSlider(),
+            entradaRadio(),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text(
+                'Guardar',
               ),
-              ElevatedButton(
-                onPressed: null,
-                child: Text('DataScreen')
-                ),
-            ],
-          )
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.next_plan_rounded),
+            label: 'Datos',
+          ),
         ],
       ),
     );
@@ -44,37 +53,41 @@ class _InputsState extends State<Inputs> {
 
   TextField entradaTexto() {
     return TextField(
-          style: AppTheme.lightTheme.textTheme.headlineMedium,
-          decoration: InputDecoration(
-            border: const UnderlineInputBorder(),
-            labelText: 'Escribe tu nombre: ',
-            labelStyle: AppTheme.lightTheme.textTheme.headlineLarge,
-          ),
-        );
+      style: AppTheme.lightTheme.textTheme.headlineMedium,
+      decoration: InputDecoration(
+        border: const UnderlineInputBorder(),
+        labelText: 'Escribe tu nombre: ',
+        labelStyle: AppTheme.lightTheme.textTheme.headlineLarge,
+      ),
+    );
   }
-  Row entradaSwitch(){
+
+  Row entradaSwitch() {
     return Row(
       children: [
+        const FlutterLogo(),
         Text(
           '¿Te gusta flutter?',
           style: AppTheme.lightTheme.textTheme.headlineLarge,
-          ),
+        ),
         Switch(
-          value: valueSwitch,
-          onChanged: (value){
-            setState(() {
-              valueSwitch = value;
-              // print('Valor del slider: $valueSwitch');
-            });
-          }
-        )
+            value: valueSwitch,
+            onChanged: (value) {
+              setState(() {
+                valueSwitch = value;
+              });
+            })
       ],
     );
   }
-  Column entradaSider(){
+
+  Column entradaSlider() {
     return Column(
       children: [
-        Text('¿Que tanto te gusta Flutter??', style: AppTheme.lightTheme.textTheme.headlineLarge,),
+        Text(
+          '¿Qué tanto te gusta flutter?',
+          style: AppTheme.lightTheme.textTheme.headlineLarge,
+        ),
         Slider(
           min: 0.0,
           max: 10.0,
@@ -82,11 +95,57 @@ class _InputsState extends State<Inputs> {
           divisions: 10,
           label: '${sliderValue.round()}',
           onChanged: (value) {
+            setState(
+              () {
+                sliderValue = value;
+                //print('Valor de Slider: $sliderValue');
+              },
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Column entradaRadio() {
+    return Column(
+      children: [
+        Text(
+          'Que prefieres usar para el desarrollo movil?',
+          style: AppTheme.lightTheme.textTheme.headlineLarge,
+        ),
+        ListTile(
+          title: Text(
+            'Tacos al pastor',
+            style: AppTheme.lightTheme.textTheme.bodySmall,
+          ),
+          leading: Radio(
+            value: 1,
+            groupValue: foodRadio,
+            onChanged: (value) {
             setState(() {
-              sliderValue = value;
-              // print('Valor del slider: $sliderValue');
+                foodRadio = value!;
+              //print('Comida seleccionada:  $foodRadio');
             });
-          }),
+            },
+          ),
+        ),
+        ListTile(
+          title: Text(
+            'Pozole',
+            style: AppTheme.lightTheme.textTheme.bodySmall,
+          ),
+          leading: Radio(
+            value: 2,
+            groupValue: foodRadio,
+            onChanged: (value) {
+            setState(() {
+                foodRadio = value!;
+              //print('Comida seleccionada:  $foodRadio');
+            });
+            },
+          ),
+        )
       ],
     );
   }
